9737b121803d008dc17fbf4573a1f57354808dbe
Elec <150059318+ElecYT@users.noreply.github.com>
2025-12-23 18:07:44 -0800

    Create build_web.ps1

---
+++ build_web.ps1
@@ -0,0 +1,13 @@
+$ErrorActionPreference = "Stop"
+$REPO_NAME = "among_us_irl"
+
+flutter pub get
+flutter build web --release --base-href "/$REPO_NAME/"
+
+if (Test-Path docs) { Remove-Item -Recurse -Force docs }
+New-Item -ItemType Directory docs | Out-Null
+Copy-Item -Recurse build\web\* docs\
+
+git add docs
+git commit -m "Deploy web" 2>$null
+git pus
