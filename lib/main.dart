b0afdaf72ff9301bfb0452ee3321422f3ee1bd62
Elec <150059318+ElecYT@users.noreply.github.com>
2025-12-24 11:19:07 -0800

    version update

--- lib/main.dart
+++ lib/main.dart
@@ -95,7 +95,7 @@ class HomeScreen extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Among Us IRL - beta 0.8.71'),
+          title: const Text('Among Us IRL - beta 0.8.72'),
           centerTitle: true,
         ),
         body: Center(
